f4e8ff76de2d4b4e287803364194e8195d5c3a9c
Debbie Cheng <dqrcheng@gmail.com>
2021-07-11 13:57:23 +1000

    Remove unused className

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,16 +2,13 @@ import React, { FC, Fragment } from 'react';
 import { GridStyled, CellStyled } from './GridStyled';
 
 const Grid: FC = () => {
-  const grid: Array<unknown[]> = Array(10).fill(null).map(() => Array(10).fill(null));
+  const grid: Array<unknown[]> = Array(10).fill(0).map(() => Array(10).fill(0));
   return (
-    <GridStyled className="grid w-96 h-96 grid-cols-10 gap-1 items-center justify-center my-10 mx-auto">
+    <GridStyled>
       {grid.map((row, y) => (
         <Fragment key={`${y * 10}`}>
           {row.map((_, x) => (
-            <CellStyled
-              key={`${y * 10 + x + 1}`}
-              className="flex items-center justify-center w-full h-full bg-gray-200 rounded-sm text-white"
-            />
+            <CellStyled key={`${y * 10 + x + 1}`} />
           ))}
         </Fragment>
       ))}
