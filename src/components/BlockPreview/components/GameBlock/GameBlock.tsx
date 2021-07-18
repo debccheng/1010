94abb8e68505113df3614d6b52be6d99190784dd
Debbie Cheng <dqrcheng@gmail.com>
2021-07-18 21:11:18 +1000

    Rearrange file structure

--- src/components/BlockPreview/components/GameBlock/GameBlock.tsx
+++ src/components/BlockPreview/components/GameBlock/GameBlock.tsx
@@ -1,6 +1,6 @@
 import React, { FC, Fragment } from 'react';
-import { Block } from '../../../utils/blockMaker';
-import ColouredCell from './ColouredCell/components/ColouredCell';
+import { Block } from '../../../../utils/blockMaker';
+import ColouredCell from './components/ColouredCell/ColouredCell';
 import { GameBlockContainerStyled } from './GameBlockStyled';
 
 const GameBlock: FC<Partial<Block>> = ({ shape, colour }: Partial<Block>) => {
